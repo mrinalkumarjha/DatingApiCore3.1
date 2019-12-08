fd3e56ca97aa1d0e8a2b45c7f2e8eba684d78d01
MITESH JHA <cool.miteshjha@gmail.com>
2019-12-08 19:54:18 +0530

    initial comit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var value = _context.Value.ToList();
+            return Ok(value);
+        }
+
+    }
+}

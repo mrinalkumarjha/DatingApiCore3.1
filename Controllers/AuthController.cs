80e0b8ef5918665cdd5daaeed58d89ec8e6c020d
MITESH JHA <cool.miteshjha@gmail.com>
2019-12-15 19:32:36 +0530

    Implemented JWT token to Login action method in Auth controller.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.Data;
 using DatingApp.Dtos;
 using DatingApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.Controllers
 {
@@ -14,24 +19,24 @@ namespace DatingApp.Controllers
     /// ControllerBase is used as base class as it is api project and we dot need view for this.
     /// </summary>
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController] // after using this we dont need to check modelstate explicitly , also no need to define [FromBody] explicitly as it maps automatically
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _confiuration;
 
         /// <summary>
         /// injecting dependency in constructor
         /// </summary>
-        public AuthController(IAuthRepository authRepository)
+        public AuthController(IAuthRepository authRepository, IConfiguration confiuration)
         {
             this._authRepository = authRepository;
+            this._confiuration = confiuration;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
-            // validate request
-
             userForRegisterDto.Username= userForRegisterDto.Username.ToLower();
             if (await _authRepository.UserExists(userForRegisterDto.Username))
                 return BadRequest("Username already exists");
@@ -45,6 +50,42 @@ namespace DatingApp.Controllers
 
             return StatusCode(201);
 
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _authRepository.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            // build token
+            // claims hold user info
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_confiuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor); // this is our jwt token
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            }); 
         }
 
     }
--- Dtos/UserForLoginDto.cs
+++ Dtos/UserForLoginDto.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace DatingApp.Dtos
 {
-    public class UserForRegisterDto
+    public class UserForLoginDto
     {
         [Required]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength =4, ErrorMessage ="You must enter password between 4 and 8")]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must enter password between 4 and 8")]
         public string Password { get; set; }
     }
 }

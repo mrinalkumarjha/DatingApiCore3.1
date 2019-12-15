37a74f9214e2c5b6db69773bc9c69687bd0b8db6
MITESH JHA <cool.miteshjha@gmail.com>
2019-12-15 15:27:12 +0530

    Added DTO for register action method.

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Data;
+using DatingApp.Dtos;
+using DatingApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    /// <summary>
+    /// ControllerBase is used as base class as it is api project and we dot need view for this.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepository;
+
+        /// <summary>
+        /// injecting dependency in constructor
+        /// </summary>
+        public AuthController(IAuthRepository authRepository)
+        {
+            this._authRepository = authRepository;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
+        {
+            // validate request
+
+            userForRegisterDto.Username= userForRegisterDto.Username.ToLower();
+            if (await _authRepository.UserExists(userForRegisterDto.Username))
+                return BadRequest("Username already exists");
+
+            var userToCcreate = new User
+            {
+                Username = userForRegisterDto.Username
+            };
+
+            var createdUser = await _authRepository.Register(userToCcreate, userForRegisterDto.Password);
+
+            return StatusCode(201);
+
+        }
+
+    }
+}

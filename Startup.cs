7579a211abb2fe9affd7fa48272376afc5ed7253
MITESH JHA <cool.miteshjha@gmail.com>
2019-12-21 15:24:44 +0530

    Added Global error handling

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using DatingApp.Data;
+using DatingApp.Helpers;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -56,6 +60,27 @@ namespace DatingApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // handling exception globally
+                app.UseExceptionHandler(builder =>
+                {
+                    // run is terminal middleware
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if(error !=null)
+                        {
+                            // added extension method to add cors header as cors error in api is misleading.
+                            context.Response.AddApplicationError(error.Error.Message);
+                            // writing error to http response 
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             app.UseRouting();

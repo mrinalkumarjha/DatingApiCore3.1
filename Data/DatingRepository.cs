dc23d9fff0dd15725d0e515cd804d7c008930b56
mrinalkumarjha1 <mrinalk@itechcanada.com>
2020-01-15 19:20:41 +0530

    Added sorting to API

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -46,7 +46,8 @@ namespace DatingApp.Data
 
         public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users =  _context.Users.Include(p => p.Photos).AsQueryable();
+            var users =  _context.Users.Include(p => p.Photos)
+                .OrderByDescending(u => u.LastActive).AsQueryable();
             users = users.Where(u => u.Id != userParams.UserId); // where clause
             users = users.Where(u => u.Gender == userParams.Gender);
 
@@ -55,6 +56,18 @@ namespace DatingApp.Data
                 var minDob = DateTime.Now.AddYears(- userParams.MaxAge -1);
                 var maxDob = DateTime.Now.AddYears(-userParams.MinAge);
                 users = users.Where(u => u.DateOfBirth >= minDob && u.DateOfBirth <= maxDob);
+            }
+            if(!string.IsNullOrEmpty(userParams.OrderBy))
+            {
+                switch(userParams.OrderBy)
+                {
+                    case "created":
+                        users = users.OrderByDescending(u => u.Created);
+                        break;
+                    default:
+                        users = users.OrderByDescending(u => u.LastActive);
+                        break;
+                }
             }
 
             return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
--- Helpers/UserParams.cs
+++ Helpers/UserParams.cs
@@ -26,5 +26,7 @@ namespace DatingApp.Helpers
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
 
+        public string OrderBy { get; set; }
+
     }
 }
